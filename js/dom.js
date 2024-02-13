9b0408e9fc82025fb098946c69a70faa14b74ec3
mister_mehedi <mehedi.hasan.the.official@gmail.com>
2024-02-13 10:53:24 +0600

    first commit

---
+++ js/dom.js
@@ -0,0 +1,33 @@
+console.log('hello from js');
+// console.log(document);
+// console.log(document.body);
+
+// let hhhh=document.getElementsByTagName('h1');
+// for(let oto of hhhh){
+//   console.log(oto);
+// }
+
+
+//document.getElementById('dom-here');
+
+
+// const places=document.getElementsByClassName('place-class');
+// for(const place of places){
+//   console.log(place);
+// }
+
+// const sections=document.querySelectorAll('section');
+// for(const section of sections){
+//   section.style.border='2px solid steelblue';
+//   section.style.marginBottom='5px';
+//   section.style.borderRadius='15px';
+//   section.style.paddingLeft='7px';
+//   section.style.backgroundColor='lightgray';
+// }
+
+// let mainContainer=document.getElementById('main-container');
+// let section=document.createElement('section');
+// let h1=document.createElement('h1');
+// h1.innerText='My Daily schedule';
+// section.appendChild(h1);
+// mainContainer.appendChild(section);// ???
